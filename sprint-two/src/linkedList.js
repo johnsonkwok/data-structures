f1e6a1d709735cf79bc6e5e632219d088106fdb2
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-08 15:42:21 -0800

    Implement portion of linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail !== null) {
+      list.head = list.tail;
+      list.tail = Node(value);
+    } else if (list.head === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
+  // if (list.head === null) {
+  //   list.tail = Node(value);
+  //   list.head = list.tail;
+  // } else {
+  //   list.tail.next = Node(value);
+  //   list.tail = ;
   };
 
   list.removeHead = function() {
+    var currHead = list.head.value;
+    list.head = list.tail;
+    return currHead;
   };
 
   list.contains = function(target) {
+    // while (target.next !== null) {
+    
+    // }
   };
 
   return list;
