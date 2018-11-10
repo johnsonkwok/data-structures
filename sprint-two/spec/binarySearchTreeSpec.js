3f9701b9c21d90cae09be30bb38abafe629a75f3
johnsonkwok <johnsonkwok1@gmail.com>
2018-11-10 14:46:53 -0800

    Implement binarySearchTree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -37,4 +37,21 @@ describe('binarySearchTree', function() {
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3, 7]);
   });
+  
+  it('should execute a callback on every value, including deeply nested values on left and right properties, in a tree using "depthFirstLog"', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(10);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(14);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(4);
+    binarySearchTree.insert(13);
+    binarySearchTree.insert(15);
+    binarySearchTree.depthFirstLog(func);
+    console.log(binarySearchTree);
+    expect(array).to.eql([5, 3, 4, 1, 10, 14, 13, 15, 6, 7]);
+  });
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,5 @@
 var BinarySearchTree = function(value) {
   var binarySearchTree = Object.create(BSTPrototype);
-//   binarySearchTree.left = {};
-//   binarySearchTree.right = {};
   binarySearchTree.value = value;
   return binarySearchTree;
 };
@@ -11,20 +9,58 @@ var BSTPrototype = {};
 BSTPrototype.insert = function(value) {
   var newBST = BinarySearchTree(value);
   if (value < this.value) {
-    this.left = newBST;
+    if (!this.left) {
+      this.left = newBST;
+    } else {
+      this.left.insert(value);
+    }
   } else if (value > this.value) {
-    this.right = newBST;
+    if (!this.right) {
+      this.right = newBST;
+    } else {
+      this.right.insert(value);
+    }
   }
 };
 
 BSTPrototype.contains = function(value) {
-
+  if (this.value === value) {
+    return true;
+  } else if (value < this.value) {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  } else {
+    if (!this.right) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  }
 };
 
 BSTPrototype.depthFirstLog = function(callback) {
-
+  callback(this.value);
+  for (var key in this) {
+    if (this[key].value) {
+      callback(this[key].value);
+    } 
+    if (this[key].right) {
+      this[key].right.depthFirstLog(callback);
+    } 
+    if (this[key].left) {
+      this[key].left.depthFirstLog(callback);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * 
+ * insert: O(n)
+ * contains: O(n)
+ * depthFirstLog: O(n)
+ * 
  */
